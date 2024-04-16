bcc217a5e1a897a1457cbf700f028bb2e7e56af6
localworkarea <localworkarea@gmail.com>
2024-04-16 23:47:45 +0300

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -3477,6 +3477,24 @@
             }));
         }
     }), 0);
+    document.addEventListener("DOMContentLoaded", (function() {
+        const videoButtons = document.querySelectorAll(".video-youtube__button");
+        videoButtons.forEach((button => {
+            button.addEventListener("click", (function() {
+                const youTubeCode = this.getAttribute("data-youtube");
+                const urlVideo = `https://www.youtube.com/embed/${youTubeCode}?rel=0&showinfo=0&autoplay=1`;
+                const iframe = document.createElement("iframe");
+                iframe.setAttribute("src", urlVideo);
+                iframe.setAttribute("allowfullscreen", "");
+                const autoplay = true;
+                if (autoplay) iframe.setAttribute("allow", "autoplay; encrypted-media");
+                const body = this.closest(".video-youtube__body");
+                body.innerHTML = "";
+                body.appendChild(iframe);
+                body.classList.add("video-added");
+            }));
+        }));
+    }));
     window["FLS"] = false;
     isWebp();
     menuInit();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,30 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const videoButtons = document.querySelectorAll('.video-youtube__button');
+
+  videoButtons.forEach(button => {
+      button.addEventListener('click', function() {
+          const youTubeCode = this.getAttribute('data-youtube');
+          const urlVideo = `https://www.youtube.com/embed/${youTubeCode}?rel=0&showinfo=0&autoplay=1`;
+
+          const iframe = document.createElement('iframe');
+          iframe.setAttribute('src', urlVideo);
+          iframe.setAttribute('allowfullscreen', '');
+
+          const autoplay = true; // включаем автовоспроизведение
+
+          if (autoplay) {
+              iframe.setAttribute('allow', 'autoplay; encrypted-media');
+          }
+
+          const body = this.closest('.video-youtube__body'); // получаем обертку видео
+          body.innerHTML = ''; // очищаем содержимое обертки
+          body.appendChild(iframe); // добавляем iframe в обертку
+          body.classList.add('video-added');
+      });
+  });
+});
